322b2f047fc49dc4b77069fdd18e025df66e888d
panda <bink@inbox.ru>
2023-09-06 16:52:51 +0300

    save

--- js/common-dist.js
+++ js/common-dist.js
@@ -1,15 +1,50 @@
 
 $(document).ready(function(){
 
-let owlMain = $('.owl-main');
+let mainSlider = $('.main-slider');
 
-owlMain.owlCarousel({
-  items: 1,
-  nav: true,
-  dots: false,
-  margin: 30
-});
+mainSlider.lightSlider({
+  item:1,
+  loop:false,
+  slideMove:2,
+  easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+  speed:600,
+  adaptiveHeight: true,
+  responsive : [
+      {
+          breakpoint:800,
+          settings: {
+              item:2,
+              slideMove:1,
+              slideMargin:6,
+            }
+      },
+      {
+          breakpoint:480,
+          settings: {
+              item:2,
+              slideMove:1
+            }
+      }
+  ]
 
+});  
+let thumbSlider = $('.thumb-slider');
+
+thumbSlider.lightSlider({
+  gallery:true,
+  item:1,
+  loop:true,
+  thumbItem:9,
+  slideMargin:0,
+  enableDrag: false,
+  currentPagerPosition:'left',
+  onSliderLoad: function(el) {
+      el.lightGallery({
+          selector: '.thumb-slider .lslide'
+      });
+  }   
+});  
 
 $('.input-date').each(function(){
   let dp = new AirDatepicker(this,{
@@ -23,45 +58,6 @@ $('.input-date').each(function(){
   );
 })
 
-let slider = tns({
-  "container": '.tiny-slider',
-  "items": 3,
-  "slideBy": "page",
-  "mouseDrag": true,
-  "swipeAngle": false,
-  "speed": 400
-});
-
-let sliderVerticale = tns({
-  "container": '.tiny-slider-verticale',
-  "items": 3,
-  "axis": "vertical",
-  "slideBy": "page",
-  "mouseDrag": true,
-  "swipeAngle": false,
-  "speed": 400
-});
-
-let sliderThumbOrigin = tns({
-    "container": ".tiny-slider-thumb",
-    "items": 3,
-    "slideBy": "page",
-    "mouseDrag": true,
-    "swipeAngle": false,
-    "speed": 400
-});
-let sliderThumb = tns({
-    "container": ".tiny-slider-thumb-in",
-    "items": 3,
-    "controlsContainer": "#customize-controls",
-    "navContainer": ".tiny-slider-thumb-in",
-    "navAsThumbnails": true,
-    "autoplay": true,
-    "autoplayTimeout": 1000,
-    "autoplayButton": "#customize-toggle",
-    "swipeAngle": false,
-    "speed": 400
-});
 
 
 
